f09d9a064d5a1eb342ac048e1be40fe6c9dca9b2
AmiraliAkhavan <111018923+AmiraliAkhavan@users.noreply.github.com>
2025-08-30 15:04:32 +0330

    navbar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import "./globals.css";
 import { Figtree } from "next/font/google";
 import localFont from "next/font/local";
+import Footer from "./_components/footer/footer";
 
 const figtree = Figtree({
   display: "swap",
@@ -51,17 +52,16 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html dir="rtl" className={`${figtree.variable} ${iranyekan.variable}`}>
-      <body className="flex flex-col min-h-screen fond-bold uppercase">
-        <header className="bg-gray-200 flex items-center justify-center h-20">
-          دوره ری اکت و نکست
-        </header>
+    <html
+      dir="rtl"
+      className={`dark ${figtree.variable} ${iranyekan.variable}`}
+    >
+      <body className="grid min-h-screen grid-rows-[80px_1fr_auto] dark:bg-base-100 dark:text-base-content">
+        <Header />
         <div className="flex-1 flex items-center justify-center">
           {children}
         </div>
-        <footer className="bg-gray-200 flex items-center justify-center h-20">
-          Footer
-        </footer>
+        <Footer />
       </body>
     </html>
   );
