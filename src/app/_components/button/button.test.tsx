a07e424e2ddf671a1a135a7c9dc2b351b1f39c97
AmiraliAkhavan <111018923+AmiraliAkhavan@users.noreply.github.com>
2025-09-17 18:39:17 +0330

    jest tests

--- src/app/_components/button/button.test.tsx
+++ src/app/_components/button/button.test.tsx
@@ -1,3 +1,26 @@
-describe("Button", () => {
-  it("testing button", () => {});
+import { Button } from "./button";
+import { render, screen } from "@testing-library/react";
+
+describe("Button Component", () => {
+  test("renders a default button", () => {
+    const { getByText } = render(
+      <Button className={""} variant={"neutral"}>
+        Click Here
+      </Button>
+    );
+    expect(getByText("Click Here")).toBeInTheDocument();
+  });
+
+  test("disables the button when isDisabled prop is true", () => {
+    render(<Button isDisabled>Click here</Button>);
+    expect(screen.getByRole("button")).toBeDisabled();
+  });
+
+  test("applies the correct css class for different button variants", () => {
+    const { rerender } = render(<Button variant="primary">Click here</Button>);
+    expect(screen.getByRole("button")).toHaveClass("btn-primary");
+
+    rerender(<Button variant="info">Click here</Button>);
+    expect(screen.getByRole("button")).toHaveClass("btn-info");
+  });
 });
