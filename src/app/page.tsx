402add29a41ebb3601e062d88d536f6237bcd3c1
AmiraliAkhavan <111018923+AmiraliAkhavan@users.noreply.github.com>
2025-09-20 14:00:45 +0330

    heroSection

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,44 +1,21 @@
-import Image from "next/image";
-import { Button } from "./_components/button";
+import { CourseSummary } from "@/types/course-summary.interface";
+import { HomeHeroSection } from "./_components/home-hero-section/homeHeroSection";
 
-export default function Home() {
+async function getNewestCourses(count: number): Promise<CourseSummary[]> {
+  const res = await fetch(
+    `https://api.classbon.com/api/courses/newest/${count}`
+  );
+  return res.json();
+}
+
+export default async function Home() {
+  const newestCourses = await getNewestCourses(4);
   return (
     <>
-      <section className="bg-hero-pattern bg-no-repeat bg-center mt-5 xl:mt-20 xl:bg-left">
-        <div className="items-center flex flex-col-reverse xl:flex-row container">
-          <div className="flex flex-col gap-5 mt-12 pb-5 text-center xl:text-right ">
-            <h3 className="text-xl dark:text-info xl:text-2xl">خوش اومدی به</h3>
-            <h1 className="text-3xl font-black gradient lg:text-3xl xl:text-5xl">
-              مسیر صعود به قله های برنامه نویسی
-            </h1>
-            <p>
-              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quas
-              quam iure molestias rem aspernatur ut!
-            </p>
-            <div className="mt-5 flex gap-4">
-              <Button variant="primary" size="large">
-                دوره های ری اکت و نکست
-              </Button>
-              <Button variant="neutral" size="large">
-                مشاوره برنامه نویسی
-              </Button>
-            </div>
-            <Image
-              src="/images/frameworks.png"
-              className="mt-4 opacity-70 grayscale m-auto xl:m-0"
-              width={412}
-              height={39}
-              alt=""
-            />
-          </div>
-          <Image
-            src="/images/programmer-landing.svg"
-            alt="classbon"
-            width={702}
-            height={521}
-          />
-        </div>
-      </section>
+      <HomeHeroSection />
+      {newestCourses.map((course) => (
+        <p key={course.title}>{course.title}</p>
+      ))}
     </>
   );
 }
